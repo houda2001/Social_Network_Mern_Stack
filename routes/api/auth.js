f52051c4edf94925aef35a357fea572c15668f33
houda2001 <81920037+houda2001@users.noreply.github.com>
2022-08-16 00:11:56 +0100

    create and get profile

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,9 @@ const express=require('express');
 const router=express.Router();
 const auth = require('../../middleware/auth')
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 //RETURN USER INFORMATIONS
 router.get('/',
@@ -15,5 +18,56 @@ async(req,res)=>{
         res.status(500).json({msg:'Server Error'})
     }
 })
+
+//LOGIN 
+
+router.post(
+    "/",
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      const {email, password } = req.body;
+      try {
+
+        //see if the user exist
+        let user = await User.findOne({ email });
+        if (!user) {
+          return res.status(404).json({ errors: [{ msg: "No such a user with this email" }] });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        //check if the password is valid
+        if(!isMatch)
+        {
+          res.status(400).json({msg:'Password is not valid'})  
+        }
+         //send the jwt to the user
+        const payload={
+          user:{
+            id:user.id
+          }
+        };
+        jwt.sign(payload,
+        process.env.JWT_SECRET_KEY,
+        {expiresIn:360000},
+        (err,token)=>{
+        if(err) throw err;
+        res.json({token})
+        }
+        );
+        
+      } catch (err) {
+        console.log(err.message);
+        res.status(500).send("server error");
+      }
+    }
+  );
 
 module.exports=router;
---
+++ routes/api/profile.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/profile");
+
+//GET MY PROFILE
+//ACCESS PRIVATE
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      res.send(404).json({ msg: "No such profile" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.json({ msg: "Server Error" });
+  }
+});
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "You're status is required").notEmpty(),
+      check("skills", "Skills are required").notEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // destructure the request
+    const {
+      website,
+      skills,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+      // spread the rest of the fields we don't need to check
+      ...rest
+    } = req.body;
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (status) profileFields.status = status;
+    if (bio) profileFields.bio = bio;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+    ProfileFields.social = {};
+    if (youtube) ProfileFields.social.youtube = youtube;
+    if (facebook) ProfileFields.social.facebook = facebook;
+    if (twitter) ProfileFields.social.twitter = twitter;
+    if (linkedin) ProfileFields.social.linkedin = linkedin;
+    if (instagram) ProfileFields.social.instagram = instagram;
+
+    //Update the profileFields if the profile exist
+
+    const profile = await Profile.findOne({ user: req.user.id });
+    try {
+      if (profile) {
+        profile = await Profile.findByIdAndUpdate(
+          { user: req.user.id },
+          { $set: ProfileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+      profile = await new Profile(ProfileFields);
+      res.json(profile)
+
+    } catch (error) {}
+  }
+);
